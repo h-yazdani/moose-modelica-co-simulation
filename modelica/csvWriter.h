1570d29a4494edfb96c2c6645d72f809e12dbf1e
h-yazdani <hamedme.yazdani@gmail.com>
2026-09-18 13:56:22 +0200

    Create csvWriter.h

---
+++ modelica/csvWriter.h
@@ -0,0 +1,9 @@
+#ifndef CSVWRITER_H_
+#define CSVWRITER_H_
+
+// Function to append a row with five values to a CSV file
+// It takes the filename and five column values.
+void csvwriter(const char *filename, double col1, double col2, double col3, double col4, double col5);
+
+#endif  // CSVWRITER_H_
+
